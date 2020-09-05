cf1fd9a04409351a57585103e56c74fa96dabf64
Rahul-Chauhan21 <crahul1721@gmail.com>
2020-09-05 20:19:52 +0530

    Getting Auth Token from Spotify

---
+++ src/Config/spotify.js
@@ -0,0 +1,24 @@
+const authEndpoint = "https://accounts.spotify.com/authorize";
+const redirectUri = "http://localhost:3000/";
+const clientID = "943b60e78c8546a4b3322792da94174c";
+
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+];
+export const getTokenFromURL = () => {
+  return window.location.hash
+    .substring(1)
+    .split("&")
+    .reduce((initial, item) => {
+      let parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+      return initial;
+    }, {});
+};
+export const accessUrl = `${authEndpoint}?client_id=${clientID}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
