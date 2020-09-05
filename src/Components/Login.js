db00de8a9d1d3b4947fcc7dc228489f27b0940b7
Rahul-Chauhan21 <crahul1721@gmail.com>
2020-09-05 20:18:35 +0530

    Added AccessUrl from Spotify Auth

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Login.css";
+import { accessUrl } from "../Config/spotify";
 function Login() {
   return (
     <div className="login">
@@ -7,7 +8,7 @@ function Login() {
         src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
         alt=""
       />
-      <button>LOGIN WITH SPOTIFY</button>
+      <a href={accessUrl}>LOGIN WITH SPOTIFY</a>
     </div>
   );
 }
