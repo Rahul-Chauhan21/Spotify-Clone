a275e151569e346f65db988117e4774891091d44
Rahul-Chauhan21 <crahul1721@gmail.com>
2020-09-05 20:21:16 +0530

    Login Page Implemented

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Login from "./Components/Login";
+import Player from "./Components/Player";
 import "./App.css";
+import { getTokenFromURL } from "./Config/spotify";
 
 function App() {
-  return (
-    <div className="App">
-      <Login />
-    </div>
-  );
+  const [token, setToken] = useState(null);
+  // Run code based on a given condition
+  useEffect(() => {
+    const hash = getTokenFromURL();
+    window.location.hash = "";
+
+    const _token = hash.access_token;
+    if (_token) {
+      setToken(_token);
+    }
+  }, []);
+  return <div className="App">{token ? <Player /> : <Login />}</div>;
 }
 
 export default App;
