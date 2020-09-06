b243f9a77e2be5d0c9e4408fb97aaeec9082f577
Rahul-Chauhan21 <crahul1721@gmail.com>
2020-09-07 00:29:44 +0530

    Using DataLayer to Alter State

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
+import SpotifyWebApi from "spotify-web-api-js";
 import Login from "./Components/Login";
 import Player from "./Components/Player";
 import "./App.css";
 import { getTokenFromURL } from "./Config/spotify";
+import { useDataLayerValue } from "./DataLayer.js";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
-  const [token, setToken] = useState(null);
+  const [{ token, user }, dispatch] = useDataLayerValue();
   // Run code based on a given condition
   useEffect(() => {
     const hash = getTokenFromURL();
@@ -13,10 +17,26 @@ function App() {
 
     const _token = hash.access_token;
     if (_token) {
-      setToken(_token);
+      //setToken(_token);
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      });
+      spotify.setAccessToken(_token); // giving access token to Spotify API
+      spotify.getMe().then((user) => {
+        dispatch({
+          type: "SET_USER",
+          user: user,
+        });
+      });
     }
   }, []);
-  return <div className="App">{token ? <Player /> : <Login />}</div>;
+
+  return (
+    <div className="App">
+      {token ? <Player spotify={spotify} /> : <Login />}
+    </div>
+  );
 }
 
 export default App;
