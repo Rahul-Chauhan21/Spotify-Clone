698c0af4b3081203a7e5b70adbc3ed02c9668149
Rahul-Chauhan21 <crahul1721@gmail.com>
2020-09-13 20:38:41 +0530

    Rendering Currently playing track info

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
 import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
 import SkipNextIcon from "@material-ui/icons/SkipNext";
@@ -9,15 +9,43 @@ import PauseCircleOutlineIcon from "@material-ui/icons/PauseCircleOutline";
 import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
 import "./Footer.css";
 import { Grid, Slider } from "@material-ui/core";
-function Footer() {
+import { useDataLayerValue } from "../DataLayer";
+function Footer({ spotify }) {
+  const [{ token, item, playing }, dispatch] = useDataLayerValue();
+  useEffect(() => {
+    spotify.getMyCurrentPlaybackState().then((res) => {
+      console.log("Current playback is", res);
+
+      dispatch({
+        type: "SET_PLAYING",
+        playing: res.is_playing,
+      });
+
+      dispatch({
+        type: "SET_ITEM",
+        item: res.item,
+      });
+    });
+  }, [spotify]);
   return (
     <div className="footer">
       <div className="footer__left">
-        <img className="footer_ablumLogo" src="" alt="" />
-        <div className="footer__songInfo">
-          <h4>No song is playing</h4>
-          <p>...</p>
-        </div>
+        <img
+          className="footer__albumLogo"
+          src={item?.album.images[0].url}
+          alt={item?.name}
+        />
+        {item ? (
+          <div className="footer__songInfo">
+            <h4>{item.name}</h4>
+            <p>{item.artists.map((artist) => artist.name).join(", ")}</p>
+          </div>
+        ) : (
+          <div className="footer__songInfo">
+            <h4>No song is playing</h4>
+            <p>...</p>
+          </div>
+        )}
       </div>
       <div className="footer__center">
         <ShuffleIcon className="footer__green" />
