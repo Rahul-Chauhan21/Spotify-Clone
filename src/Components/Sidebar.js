9fee12f8fafc8ab58c1a45aed8a071eed9730d5c
Rahul-Chauhan21 <crahul1721@gmail.com>
2020-09-13 20:39:55 +0530

    Passing id prop

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -6,21 +6,21 @@ import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
 import SidebarOption from "./SidebarOption";
 import { useDataLayerValue } from "../DataLayer";
 function Sidebar() {
-  const [{ playlists }, dispatch] = useDataLayerValue();
+  const [{ playlists }] = useDataLayerValue();
   return (
     <div className="sidebar">
       <img
         className="sidebar__logo"
         src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
       />
-      <SidebarOption Icon={HomeIcon} title="Home" />
-      <SidebarOption Icon={SearchIcon} title="Search" />
-      <SidebarOption Icon={LibraryMusicIcon} title="Your Library" />
+      <SidebarOption Icon={HomeIcon} title="Home" id={null} />
+      <SidebarOption Icon={SearchIcon} title="Search" id={null} />
+      <SidebarOption Icon={LibraryMusicIcon} title="Your Library" id={null} />
       <br />
       <strong className="sidebar__title">PLAYLISTS</strong>
       <hr />
       {playlists?.items?.map((playlist) => (
-        <SidebarOption title={playlist.name} />
+        <SidebarOption title={playlist.name} id={playlist.id} />
       ))}
     </div>
   );
