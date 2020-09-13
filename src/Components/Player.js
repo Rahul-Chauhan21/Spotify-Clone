2343e1c16c66c72078fa55db46752ce7a066f175
Rahul-Chauhan21 <crahul1721@gmail.com>
2020-09-13 20:39:20 +0530

    Passing spotify obj prop

--- src/Components/Player.js
+++ src/Components/Player.js
@@ -3,15 +3,15 @@ import Sidebar from "./Sidebar";
 import Body from "./Body";
 import Footer from "./Footer";
 import "./Player.css";
-function Player() {
+function Player({ spotify }) {
   return (
     <div className="player">
       <div className="player__body">
         <Sidebar />
-        <Body />
+        <Body spotify={spotify} />
       </div>
 
-      <Footer />
+      <Footer spotify={spotify} />
     </div>
   );
 }
