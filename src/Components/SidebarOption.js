883652dfb26ea67c8dfa34ffa19562b9bfceee8b
Rahul-Chauhan21 <crahul1721@gmail.com>
2020-09-13 20:41:06 +0530

    dispatching id prop to get current playlist info

--- src/Components/SidebarOption.js
+++ src/Components/SidebarOption.js
@@ -1,8 +1,20 @@
 import React from "react";
+import { useDataLayerValue } from "../DataLayer";
 import "./SidebarOption.css";
-function SidebarOption({ title, Icon }) {
+function SidebarOption({ title, Icon, id }) {
+  const [{ spotify }, dispatch] = useDataLayerValue();
+  const handleClick = () => {
+    if (id) {
+      spotify.getPlaylist(id).then((response) => {
+        dispatch({
+          type: "SET_DISCOVER_WEEKLY",
+          discover_weekly: response,
+        });
+      });
+    }
+  };
   return (
-    <div className="sidebarOption">
+    <div className="sidebarOption" onClick={handleClick}>
       {Icon && <Icon className="sidebarOption__icon" />}
       {Icon ? <h4>{title}</h4> : <p> {title}</p>}
     </div>
