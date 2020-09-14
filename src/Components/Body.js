eddc795bde6abccf2196df40b84d143c4cbc7b76
Rahul-Chauhan21 <crahul1721@gmail.com>
2020-09-14 20:05:50 +0530

    Changes Made

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -6,30 +6,87 @@ import FavoriteIcon from "@material-ui/icons/Favorite";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 import { useDataLayerValue } from "../DataLayer";
 import SongRow from "./SongRow";
+import Artists from "./Artists";
 
 function Body({ spotify }) {
   const [{ discover_weekly }, dispatch] = useDataLayerValue();
+  const playPlaylist = () => {
+    spotify
+      .play({
+        context_uri: `spotify:playlist:${discover_weekly.id}`,
+      })
+      .then((res) => {
+        spotify.getMyCurrentPlayingTrack().then((res) => {
+          dispatch({
+            type: "SET_ITEM",
+            item: res.item,
+          });
+          dispatch({
+            type: "SET_PLAYING",
+            playing: true,
+          });
+        });
+      });
+  };
+  const playSong = (id) => {
+    console.log(id);
+    spotify
+      .play({
+        uris: [`spotify:track:${id}`],
+      })
+      .then((res) => {
+        spotify.getMyCurrentPlayingTrack().then((res) => {
+          dispatch({
+            type: "SET_ITEM",
+            item: res.item,
+          });
+          dispatch({
+            type: "SET_PLAYING",
+            playing: true,
+          });
+        });
+      });
+  };
   return (
     <div className="body">
       <Header spotify={spotify} />
-      <div className="body__info">
-        <img src={discover_weekly?.images[0].url} alt="" />
-        <div className="body__infoText">
-          <strong>PLAYLIST</strong>
-          <h2>{discover_weekly?.name}</h2>
-          <p>{discover_weekly?.description}</p>
-        </div>
-      </div>
-      <div className="body__songs">
-        <div className="body__icons">
-          <PlayCircleFilledIcon className="body__shuffle" />
-          <FavoriteIcon fontSize="large" />
-          <MoreHorizIcon />
-        </div>
-        {discover_weekly?.tracks.items.map((item) => (
-          <SongRow track={item.track} />
-        ))}
-      </div>
+      {discover_weekly ? (
+        <>
+          <div className="body__info">
+            <img src={discover_weekly?.images[0].url} alt="" />
+            <div className="body__infoText">
+              <strong>PLAYLIST</strong>
+
+              <h2>{discover_weekly.name}</h2>
+
+              <p>{discover_weekly?.description}</p>
+            </div>
+          </div>
+          <div className="body__songs">
+            <div className="body__icons">
+              <PlayCircleFilledIcon
+                className="body__shuffle"
+                onClick={discover_weekly ? playPlaylist : null}
+              />
+              <FavoriteIcon fontSize="large" />
+              <MoreHorizIcon />
+            </div>
+            {discover_weekly?.tracks.items.map((item) =>
+              item.track ? (
+                <SongRow
+                  key={item.track.id}
+                  playSong={playSong}
+                  track={item.track}
+                />
+              ) : (
+                <div key={Date() + Math.random()}></div>
+              )
+            )}
+          </div>
+        </>
+      ) : (
+        <Artists />
+      )}
     </div>
   );
 }
